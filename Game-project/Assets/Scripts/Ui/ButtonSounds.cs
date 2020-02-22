4f610b6bed59526bf330165ea5f6a475692c3d96
kasidit-code <58929457+kasidit-code@users.noreply.github.com>
2020-02-22 21:53:25 +0700

    ซ่อมแล้วนะแง

---
+++ Game-project/Assets/Scripts/Ui/ButtonSounds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSounds : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip click, hover;
+    private AudioSource source;
+    // Start is called before the first frame update
+    void Start() {
+        source = GetComponent<AudioSource>();
+    }
+    public void DoHover() {
+        source.PlayOneShot(hover);
+    }
+
+    public void DoClick() {
+        source.PlayOneShot(click);
+    }
+}
