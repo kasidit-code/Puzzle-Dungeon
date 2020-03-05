8c9699f69b8db191bef062aba1e60c6f6eb6fd1e
kasidit-code <58929457+kasidit-code@users.noreply.github.com>
2020-03-05 16:52:20 +0700

    Enemy Health System feature <3 !!! also update player prefabs

--- Game-project/Assets/Scripts/Game/log.cs
+++ Game-project/Assets/Scripts/Game/log.cs
@@ -50,22 +50,22 @@ public class Log : Enemy
         }
     }
 
-    private void setAnimFloat(Vector2 setVector) {
+    private void SetAnimFloat(Vector2 setVector) {
         anim.SetFloat("moveX", setVector.x);
         anim.SetFloat("moveY", setVector.y);
     }
     private void changeAnim(Vector2 direction) {
         if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y)){
             if(direction.x > 0){
-                setAnimFloat(Vector2.right);
+                SetAnimFloat(Vector2.right);
             }else if(direction.x < 0){
-                setAnimFloat(Vector2.left);
+                SetAnimFloat(Vector2.left);
             }
         }else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y)){
             if(direction.y > 0){
-                setAnimFloat(Vector2.up);
+                SetAnimFloat(Vector2.up);
             }else if(direction.y < 0){
-                setAnimFloat(Vector2.down);
+                SetAnimFloat(Vector2.down);
             }
         }
     }
