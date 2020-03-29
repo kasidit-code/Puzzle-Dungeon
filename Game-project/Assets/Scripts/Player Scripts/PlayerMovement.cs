cf2063569b2582f5538de1a4dc0883eaf866197e
kasidit-code <58929457+kasidit-code@users.noreply.github.com>
2020-03-29 18:12:03 +0700

    player's damaged effect and decoration on cave

--- Game-project/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Game-project/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -22,6 +22,13 @@ public class PlayerMovement : MonoBehaviour
     public Inventory playerInventory;
     public SpriteRenderer receiveItemSprite;
 
+    public Color flashColor;
+    public Color regularColor;
+    public float flashDuration;
+    public int numberOffFlashes;
+    public Collider2D triggerCollider;
+    public SpriteRenderer mySprite;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -114,10 +121,26 @@ public class PlayerMovement : MonoBehaviour
     {
         if(myRigidbody != null)
         {
+            StartCoroutine(FlashCo());
             yield return new WaitForSeconds(knockTime);
             myRigidbody.velocity = Vector3.zero;
             currentState = PlayerState.idle;
             myRigidbody.velocity = Vector3.zero;
         }
+    }
+
+    private IEnumerator FlashCo()
+    {
+        int temp = 0;
+        triggerCollider.enabled = false;
+        while(temp < numberOffFlashes)
+        {
+            mySprite.color = flashColor;
+            yield return new WaitForSeconds(flashDuration);
+            mySprite.color = regularColor;
+            yield return new WaitForSeconds(flashDuration);
+            temp ++;
+        }
+        triggerCollider.enabled = true;
     }
 }
