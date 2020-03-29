09be9d37cb4e647d8bf5889aac6a628ea2b5607c
Chatchai-Aunjai <58930742+Chatchai-Aunjai@users.noreply.github.com>
2020-03-29 13:16:58 +0700

    Game Pause

---
+++ Game-project/Assets/Scripts/Game stuff/GameSaveManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSaveManager : MonoBehaviour
+{
+
+    public static GameSaveManager gameSave;
+    public List<ScriptableObject> objects = new List<ScriptableObject>();
+
+    private void Awake()
+    {
+        if (gameSave = null)
+        {
+            gameSave = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Game-project/Assets/Scripts/Game stuff/PauseManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public Animator animator;
+    private bool isPaused;
+    public GameObject pausePanel;
+    public int movedScene;
+    private int sceneToload;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("pause"))
+        {
+            Resume();
+        }
+    }
+
+    public void Resume()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Ouit(string sceneName)
+    {
+        FadeToScene(movedScene);
+        Resume();
+    }
+
+    public void FadeToScene(int sceneName) {
+        sceneToload = sceneName;
+        animator.SetTrigger("FadeOut");
+    }
+    
+    public void onFadeComplete() {
+        SceneManager.LoadScene(sceneToload);
+    }
+    
+}
